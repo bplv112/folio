3507bc1e4799e1761980c16eda9ea9d4aa53146d
bplv <bplv.me@gmail.com>
2023-08-26 17:34:16 -0500

    Updated :: Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,7 +8,7 @@ function Header() {
 			<header className='header sm:right-0 sm:bottom-10-percent'>
 				<div className="info">
 					<h1 className='max-sm:text-xl text-6xl'> Howdy, I am <span className="bg-pr-3 font-press-start text-3xl p-3 max-sm:text-xl">Biplav<span className="blink text-3xl max-sm:text-xl">!</span></span></h1>
-					<p className='py-1 mt-3 text-xl max-sm:text-md'> Full stack Web Engineer && in-shower life contemplator.</p>
+					<p className='py-1 mt-3 text-xl max-sm:text-md'> Full stack Web Engineer</p>
 					<p className='hidden py-1 mt-1 max-sm:flex'> Hit me on up on my socials! </p>
 				</div>
 			</header>
